e0e41194c718612bf48fa2cf0c5f235d9263cd78
Mohamed, Belhadi <mbelhadi@isndev.com>
2020-11-12 04:35:54 +0100

    [dev] added async::Request, updated ClientActor

--- actor/ClientActor.h
+++ actor/ClientActor.h
@@ -17,7 +17,6 @@
 
 #include <qb/actor.h>
 #include <qb/io/async.h>
-#include <qb/io/async/tcp/connector.h>
 
 #include "../http.h"
 #include "../tag/ClientTag.h"
@@ -30,75 +29,6 @@
 namespace qb::http {
     class ClientActor
             : public ServiceActor<ClientTag> {
-        class ISession {
-        public:
-            virtual ~ISession() = default;
-            virtual void connect(io::uri const &) = 0;
-        };
-
-        template <typename Transport>
-        struct Session
-                : public io::async::tcp::client<Session<Transport>, Transport>
-                        , public ISession {
-            ClientActor &actor;
-            const uint64_t id_request;
-            const Request<> request;
-            const ActorId reply_to;
-
-        public:
-            using Protocol = protocol<Session<Transport>>;
-            Session(ClientActor &actor,
-                    uint64_t req_id,
-                    Request<> &request,
-                    ActorId reply)
-                    : actor(actor)
-                    , id_request(req_id)
-                    , request(std::move(request))
-                    , reply_to(reply){}
-            ~Session() = default;
-
-            void connect(qb::io::uri const &remote) final {
-                qb::io::async::tcp::connect<typename Transport::transport_io_type>(
-                        remote, [this](auto &transport) {
-                            if (!transport.is_open()) {
-                                auto &e = actor.push<ResponseEvent>(reply_to);
-                                e.id_request = id_request;
-                                e.data.response.status_code = HTTP_STATUS_SERVICE_UNAVAILABLE;
-                                LOG_WARN(actor << "[Fail] request "
-                                                  << llhttp_method_name(request.method) << " " << request.url
-                                                  << " unresolved");
-                                delete this;
-                            } else {
-                                this->transport() = transport;
-                                this->start();
-                                *this << request;
-                            }
-                        });
-            }
-
-            void on(typename Protocol::response &&event) {
-                auto &e = actor.template push<ResponseEvent>(reply_to);
-                e.id_request = id_request;
-                e.data.response = std::move(event.http);
-                this->disconnect(1);
-                LOG_INFO(actor << "[Success] request " << llhttp_method_name(request.method) << " " << request.url);
-            }
-
-            void on(qb::io::async::event::disconnected const &event) {
-                if (!event.reason) {
-                    auto &e = actor.push<ResponseEvent>(reply_to);
-                    e.id_request = id_request;
-                    e.data.response.status_code = HTTP_STATUS_REQUEST_TIMEOUT;
-                    LOG_WARN(actor << "[Fail] request "
-                                   << llhttp_method_name(request.method) << " " << request.url
-                                   << " lost connection");
-                }
-            }
-
-            void on(qb::io::async::event::dispose const &) {
-                delete this;
-            }
-        };
 
     public:
 
@@ -109,19 +39,33 @@ namespace qb::http {
         void on(RequestEvent &event) {
             const auto scheme = event.data.uri.scheme();
             const auto &uri = event.data.uri;
+            const auto reply_to = event.getSource();
+            const auto id_request = event.id_request;
             auto &request = event.data.request;
-            ISession *session = nullptr;
-
             request.url = uri.source();
-            request.path = uri.full_path();
-            request.headers["host"].emplace_back(uri.host());
 
-            if (scheme == "http") {
-                session = new Session<io::transport::tcp>(*this, event.id_request, request, event.getSource());
-            } else if (scheme == "https")
-                session = new Session<io::transport::stcp>(*this, event.id_request, request, event.getSource());
+            async::REQUEST(request, [this, id_request, reply_to](auto &&result) {
+                switch(result.response.status_code) {
+                    case HTTP_STATUS_SERVICE_UNAVAILABLE:
+                        LOG_WARN(*this << "[Fail] request "
+                                       << llhttp_method_name(result.request.method) << " " << result.request.url
+                                       << " couldn't connect");
+                        break;
+                    case HTTP_STATUS_GONE:
+                        LOG_WARN(*this << "[Fail] request "
+                                       << llhttp_method_name(result.request.method) << " " << result.request.url
+                                       << " lost connection");
+                        break;
+                    default:
+                        LOG_DEBUG(*this << "[Succes] request "
+                                       << llhttp_method_name(result.request.method) << " " << result.request.url);
+                        break;
+                }
+                auto &e = this->template push<ResponseEvent>(reply_to);
+                e.id_request = id_request;
+                e.data.response = std::move(result.response);
 
-            session->connect(uri);
+            }, 3);
         }
     };
 }
